85db040aafc420dd3279c7fbcc1745b303b117bc
ybadrodien <epigrammatik@gmail.com>
2018-04-12 13:15:15 +0200

    namechange

---
+++ Day_1_basic_stats.R
@@ -0,0 +1,9 @@
+
+# Yameen Badrodien --------------------------------------------------------
+# 12 April 2018
+# Day 1_basic_stats.R
+# Day 1 stats class
+# Purpose: Practice some of the concepts we will encounter
+
+
+
